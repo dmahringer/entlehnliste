8a03f7ab7a85dc5ae3f75132a3841884ec118614
elisa_kaltenberger <e.kaltenberger@students.htl-leonding.ac.at>
2023-11-22 14:23:29 +0100

    teacher

---
+++ my-app/src/main/java/at/ac/htl/bhitm3/model/Teacher.java
@@ -0,0 +1,10 @@
+package at.ac.htl.bhitm3.model;
+
+public class Teacher extends Person {
+    public void setSalary(double salary) {
+    }   
+
+    public Object getSalary() {
+        return null;
+    }
+}
--- my-app/src/test/java/at/ac/htl/bhitm3/model/TeacherTest.java
+++ my-app/src/test/java/at/ac/htl/bhitm3/model/TeacherTest.java
@@ -3,10 +3,21 @@ package at.ac.htl.bhitm3.model;
 import org.junit.Before;
 
 public class TeacherTest {
-    //Teacher teacher;
+    Teacher teacher;
 
     @Before
     public void setUp() {
+        teacher = new Teacher();
+    }
 
+    @Test
+    public void given_teacher_then_is_instance_of_person() {
+        assertTrue(teacher instanceof Person);
+    }
+
+    @Test
+    public void given_salary_from_teacher_must_be_more_then_zero() {
+        teacher.setSalary(2000);
+        assertEquals(2000, teacher.getName());
     }
 }
