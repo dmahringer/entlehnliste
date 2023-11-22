04c020e5f2717296ec1433812a3326dbcaeafee6
elisa_kaltenberger <e.kaltenberger@students.htl-leonding.ac.at>
2023-11-22 14:41:02 +0100

    student

--- my-app/src/main/java/at/ac/htl/bhitm3/model/Student.java
+++ my-app/src/main/java/at/ac/htl/bhitm3/model/Student.java
@@ -1,5 +1,13 @@
 package at.ac.htl.bhitm3.model;
 
 public class Student extends Person {
+    String studentID;
 
+    public void setStudentID(String id) {
+        this.studentID = id;
+    }
+
+    public Object getStudentID(
+        return
+    )
 }
