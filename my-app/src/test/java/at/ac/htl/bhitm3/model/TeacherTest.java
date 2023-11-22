629c220cf625843c87ffc2214156da486a5614e9
vcatic <vanesa.cat09@gmail.com>
2023-11-22 14:24:59 +0100

    teacher

--- my-app/src/test/java/at/ac/htl/bhitm3/model/TeacherTest.java
+++ my-app/src/test/java/at/ac/htl/bhitm3/model/TeacherTest.java
@@ -1,6 +1,10 @@
 package at.ac.htl.bhitm3.model;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
+import org.junit.Test;
 
 public class TeacherTest {
     Teacher teacher;
