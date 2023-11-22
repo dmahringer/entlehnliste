a3daac2d82b43975e9f08d64c1a552b39158bc0b
dmahringer <d.mahringer@students.htl-leonding.ac.at>
2023-11-22 14:49:34 +0100

    expanded PersonTest

--- my-app/src/test/java/at/ac/htl/bhitm3/model/PersonTest.java
+++ my-app/src/test/java/at/ac/htl/bhitm3/model/PersonTest.java
@@ -8,15 +8,30 @@ import org.junit.Test;
 public class PersonTest {
 
     Person person;
+    Address address;
 
     @Before
     public void setup() {
         person = new Person();
+        address = new Address();
     }
     
     @Test
     public void given_person_when_name_is_John() {
         person.setName("John");
         assertEquals("John", person.getName());
+    }
+
+    @Test
+    public void given_person_when_address_is_address1() {
+        address.setStreet("Limesstraße");
+        address.setHouseNumber(14);
+        address.setPLZ(4060);
+        address.setCity("Leonding");
+        assertEquals("Limesstraße", address.getStreet());
+        assertEquals(14, address.getHouseNumber());
+        assertEquals(4060, address.getPLZ());
+        assertEquals("Leonding", address.getCity());
+
     }
 }
