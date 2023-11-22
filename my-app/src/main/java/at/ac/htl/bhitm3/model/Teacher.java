32a837c23584af360974e90bff99c531e7df847e
vcatic <vanesa.cat09@gmail.com>
2023-11-22 14:26:39 +0100

    teacher

--- my-app/src/main/java/at/ac/htl/bhitm3/model/Teacher.java
+++ my-app/src/main/java/at/ac/htl/bhitm3/model/Teacher.java
@@ -1,10 +1,12 @@
 package at.ac.htl.bhitm3.model;
 
 public class Teacher extends Person {
+    double salary;
     public void setSalary(double salary) {
+        this.salary = salary;
     }   
 
     public Object getSalary() {
-        return null;
+        return this.salary;
     }
 }
